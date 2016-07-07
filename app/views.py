1bfe29ac3d52b240ed8cf9c55e68eadd81197828
blessma <blessma@schmidtelectronics.com>
2016-07-07 17:17:41 +0800

    test

--- app/views.py
+++ app/views.py
@@ -12,7 +12,26 @@ def index():
 @app.route("/callback", methods=['POST'])
 def callback():
     if request.method == 'POST':
+        url = 'https://trialbot-api.line.me/v1/events'
+        headers = {
+                   'Content-Type': 'application/json; charset=UTF-8',
+                   'X-Line-ChannelID': '1473077665',
+                   'X-Line-ChannelSecret': '8582629c37b18605491087172be06e5e',
+                   'X-Line-Trusted-User-With-ACL': 'u3425f8daf2f07f3a9d723f5232f50f63'
+                  }
+        data = {
+                'to': ['u96e32e17ebdedd21c1f84bbbfd7de08c'],
+                'toChannel': 1383378250,
+                'eventType': '138311608800106203',
+                'content': {
+                    'contentType': 1,
+                    'toType': 1,
+                    'text': u'寶寶不說'
+                    }
+               }
         js = request.get_json()
         print(type(js))
         print(len(js['result']))
-        return 'test'
+
+	r = requests.post(url, data=json.dumps(data), headers=headers)
+	return json.dumps(r.json(), indent=4)
