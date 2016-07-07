6245ef9b5ff8fe1a9a09b45c1410ca3ace9099d9
blessma <blessma@schmidtelectronics.com>
2016-07-07 15:40:49 +0800

    callback test

--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
 from flask import render_template, request, jsonify
 from app import app
 
+
 @app.route("/", methods=['GET'])
 def index():
     if request.method == 'GET':
         return render_template('main.html')
 
+
+@app.route("/", methods=['POST'])
+def callback():
+    if request.method == 'POST':
+        print(request.get_json())
+        return 'test'
