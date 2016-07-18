1c9ac330d34539a202af98c1b7f37bdaa2a81900
blessma <blessma@schmidtelectronics.com>
2016-07-18 15:38:28 +0800

    test

--- app/views.py
+++ app/views.py
@@ -11,13 +11,38 @@ sys.path.insert(0, os.path.abspath('..'))
 from flux import FLUX
 from fluxclient.robot import FluxRobot
 from fluxclient.commands.misc import get_or_create_default_key
+from fluxclient.utils._utils import Tools
 
 status_set = {'status',
               '狀態',
               '狀況',
               '進度'}
-
 FLUX_ipaddr = "122.116.80.243"
+
+def get_message(json):
+    _id, message = [json['result'][0]['content']['from']], json['result'][0]['content']['text']
+    return _id, message
+
+def send_message(to_user, content):
+    url = 'https://trialbot-api.line.me/v1/events'
+    headers = {
+               'Content-Type': 'application/json; charset=UTF-8',
+               'X-Line-ChannelID': '1473077665',
+               'X-Line-ChannelSecret': '8582629c37b18605491087172be06e5e',
+               'X-Line-Trusted-User-With-ACL': 'u3425f8daf2f07f3a9d723f5232f50f63'
+              }
+    data = {
+            'to': to_user,
+            'toChannel': 1383378250,
+            'eventType': '138311608800106203',
+            'content': {
+                'contentType': 1,
+                'toType': 1,
+                'text': content
+                }
+           }
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    return json.dumps(r.json(), indent=4)
 
 
 @app.route("/", methods=['GET'])
@@ -45,30 +70,6 @@ def add_rsa():
 
 @app.route("/callback", methods=['GET', 'POST'])
 def callback():
-    def get_message(json):
-        _id, message = [json['result'][0]['content']['from']], json['result'][0]['content']['text']
-        return _id, message
-
-    def send_message(to_user, content):
-        url = 'https://trialbot-api.line.me/v1/events'
-        headers = {
-                   'Content-Type': 'application/json; charset=UTF-8',
-                   'X-Line-ChannelID': '1473077665',
-                   'X-Line-ChannelSecret': '8582629c37b18605491087172be06e5e',
-                   'X-Line-Trusted-User-With-ACL': 'u3425f8daf2f07f3a9d723f5232f50f63'
-                  }
-        data = {
-                'to': to_user,
-                'toChannel': 1383378250,
-                'eventType': '138311608800106203',
-                'content': {
-                    'contentType': 1,
-                    'toType': 1,
-                    'text': content
-                    }
-               }
-        r = requests.post(url, data=json.dumps(data), headers=headers)
-        return json.dumps(r.json(), indent=4)
 
     if request.method == 'POST':
         js = request.get_json()
