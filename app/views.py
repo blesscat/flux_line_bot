e1a4e907d3d59042a7fa01cb88536b8bf87c429a
blessma <blessma@schmidtelectronics.com>
2016-07-26 23:28:53 +0800

    test

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ import requests
 import json
 import socket
 import math
+import time
 from flask import render_template, request
 from app import app
 
@@ -14,41 +15,54 @@ from flux import FLUX
 from fluxclient.robot import FluxRobot, errors
 from fluxclient.commands.misc import get_or_create_default_key
 
-start_list = {'start',
-              'START',
-              '開始'}
-
-pause_list = {'pause',
-              'PAUSE',
-              '暫停'}
-
-resume_list = {'resume',
-               'RESUME',
-               '繼續'}
-
-abort_list = {'abort',
-              'ABORT',
-              'stop',
-              'STOP',
-              '停止',
-              '結束'}
-
-list_files_set = {'list',
-                  'LIST',
-                  '檔案'}
-
-status_set = {'200',
+status_set = {'110',
               'status',
               'STATUS',
               '狀態',
               '狀況',
               '進度'}
 
+list_files_set = {'120',
+                  'list',
+                  'LIST',
+                  '檔案'}
+
+start_set = {'210',
+             'start',
+             'START',
+             '開始'}
+
+pause_set = {'220',
+             'pause',
+             'PAUSE',
+             '暫停'}
+
+resume_set = {'230',
+              'resume',
+              'RESUME',
+              '繼續'}
+
+abort_set = {'240',
+             'abort',
+             'ABORT',
+             'stop',
+             'STOP',
+             '停止',
+             '結束'}
+
+FLUX_COMMANDS=""
+flux_command_list = ["110 - status",
+                "120 - list_files",
+                "210 - start",
+                "220 - pause",
+                "230 - resume",
+                "240 - abort"]
+for command in flux_command_list:
+    FLUX_COMMANDS += command + '\n'
 
 FLUX_ipaddr = socket.gethostbyname(os.environ['FLUX_ipaddr'])
 MANTRA = os.environ['mantra']
 NAME = os.environ['name']
-
 
 def isin(message, message_set):
     _bool = bool({status for status in message_set if status in message})
@@ -109,103 +123,126 @@ def isin_status(Flux):
                    MANTRA, label, prog, leftTime)
     return message
 
+
+def isin_pause(Flux):
+    try:
+        Flux.pause_play()
+        message = '{}\n已經暫停了喔'.format(MANTRA)
+    except:
+        message = '{}\n無法暫停，可能已經停止了'.format(MANTRA)
+    return message
+
+
+def isin_resume(Flux):
+    try:
+        Flux.resume_play()
+        message = '{}\n已經繼續在印了呢'.format(MANTRA)
+    except:
+        message = '{}\n無法繼續，可能已經停止了'.format(MANTRA)
+    return message
+
+
+def isin_abort(Flux):
+    try:
+        Flux.abort_play()
+        message = '{}\n已經停止囉'.format(MANTRA)
+    except:
+        message = '{}\n無法停止，可能早就已經停止了呢'.format(MANTRA)
+    return message
+
+
+def isin_list_files(Flux):
+    _list = str(Flux.list_files('/SD/Recent'))
+    message = '{}'.format(_list)
+    return message
+
+
 def send_message(to_user, content):
-    url = 'https://trialbot-api.line.me/v1/events'
-    headers = {
-               'Content-Type': 'application/json; charset=UTF-8',
-               'X-Line-ChannelID': os.environ['ChannelID'],
-               'X-Line-ChannelSecret': os.environ['ChannelSecret'],
-               'X-Line-Trusted-User-With-ACL': os.environ['MID']
-              }
-    data = {
-            'to': to_user,
-            'toChannel': 1383378250,
-            'eventType': '138311608800106203',
-            'content': {
-                'contentType': 1,
-                'toType': 1,
-                'text': content
-                }
-           }
-    r = requests.post(url, data=json.dumps(data), headers=headers)
-    return json.dumps(r.json(), indent=4)
+   url = 'https://trialbot-api.line.me/v1/events'
+   headers = {
+              'Content-Type': 'application/json; charset=UTF-8',
+              'X-Line-ChannelID': os.environ['ChannelID'],
+              'X-Line-ChannelSecret': os.environ['ChannelSecret'],
+              'X-Line-Trusted-User-With-ACL': os.environ['MID']
+             }
+   data = {
+           'to': to_user,
+           'toChannel': 1383378250,
+           'eventType': '138311608800106203',
+           'content': {
+               'contentType': 1,
+               'toType': 1,
+               'text': content
+               }
+          }
+   r = requests.post(url, data=json.dumps(data), headers=headers)
+   return json.dumps(r.json(), indent=4)
 
 
-@app.route("/", methods=['GET'])
+#app.route("/", methods=['GET'])
 def index():
-    if request.method == 'GET':
-        return render_template('main.html')
+   if request.method == 'GET':
+       return render_template('main.html')
 
 
 @app.route("/test", methods=['GET'])
 def test():
-    if request.method == 'GET':
-        print(type(os.environ['test12']))
-        return str(os.environ['test12'])
+   if request.method == 'GET':
+       print(type(os.environ['test12']))
+       return str(os.environ['test12'])
 
 
 @app.route("/g2ftest", methods=['GET'])
 def g2ftest():
-    if request.method == 'GET':
-        os.system("flux_g2f -i tset.gcode -o test.fc")
-        ls = os.popen("ls")
-        print(ls)
-        return str(ls)
+   if request.method == 'GET':
+       os.system("flux_g2f -i tset.gcode -o test.fc")
+       ls = os.popen("ls")
+       print(ls)
+       return str(ls)
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
     if request.method == 'POST':
         js = request.get_json()
-        print(js)
         _id, message = get_message(js)
         if message == '罐罐':
             message = '{0}要吃罐罐！！\n{0}要吃罐罐！！\n給{0}吃！！'.format(NAME)
+            send_message(_id, message)
+            time.sleep(5)
+            message = '{}\n我能幫你做的事情如下喔\n{}'.format(MANTRA, NAME, FLUX_COMMANDS)
             send_message(_id, message)
             return 'ok'
         
         magic_id = message[:5].lower()
         if magic_id == 'flux ' or magic_id == '8763 ':
             Flux = robot()
-
+ 
             if isin(message, status_set):
                 message = isin_status(Flux)
-
-            elif isin(message, start_list):
-                    message = '{}\n開始功能還沒完成喔～～'.format(MANTRA)
-
-            elif isin(message, pause_list):
-                try:
-                    Flux.pause_play()
-                    message = '{}\n已經暫停了喔'.format(MANTRA)
-                except:
-                    message = '{}\n無法暫停，可能已經停止了'.format(MANTRA)
-
-            elif isin(message, resume_list):
-                try:
-                    Flux.resume_play()
-                    message = '{}\n已經繼續在印了呢'.format(MANTRA)
-                except:
-                    message = '{}\n無法繼續，可能已經停止了'.format(MANTRA)
-
-            elif isin(message, abort_list):
-                try:
-                    Flux.abort_play()
-                    message = '{}\n已經停止囉'.format(MANTRA)
-                except:
-                    message = '{}\n無法停止，可能早就已經停止了呢'.format(MANTRA)
-
+ 
             elif isin(message, list_files_set):
-                _list = str(Flux.list_files('/SD/Recent'))
-                message = '{}'.format(_list)
+                message = isin_list_files(Flux)
 
+            elif isin(message, start_set):
+                message = '{}\n開始功能還沒完成喔～～'.format(MANTRA)
+ 
+            elif isin(message, pause_set):
+                message = isin_pause(Flux)
+ 
+            elif isin(message, resume_set):
+                message = isin_resume(Flux)
+ 
+            elif isin(message, abort_set):
+                message = isin_abort(Flux)
+ 
             else :
                 message = '{}\n{}不知道"{}"是什麼啦！'.format(MANTRA, NAME, message[5:])
-
+ 
             send_message(_id, message)
             Flux.close()
             return 'ok'
-
+ 
         else:
             message = '{0}知道什麼是"{1}"，但是{0}不說'.format(NAME, message)
             send_message(_id, message)
