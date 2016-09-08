162d0a442e723999225c6efe21a961f9e8d3cdd5
blessma <blessma@schmidtelectronics.com>
2016-09-08 22:31:49 +0800

    test

--- app/backend.py
+++ app/backend.py
@@ -2,12 +2,17 @@ import os
 import sys
 import time
 import threading
+from app import line
 
 sys.path.insert(0, os.path.abspath('..'))
 
 from flux import FLUX
 from fluxclient.robot import FluxRobot, errors
 from fluxclient.commands.misc import get_or_create_default_key
+
+_id_ = os.environ.get('LineID', 'none')
+_id = [_id_]
+MANTRA = os.environ['mantra']
 
 
 class load_filament_backend(threading.Thread):
@@ -18,6 +23,8 @@ class load_filament_backend(threading.Thread):
     def run(self):
         main = self.Flux.maintain()
         try:
+            message = '{}\nFLUX加溫中～～'.format(MANTRA)
+            line.send_message(_id, message)
             def callback(robot_connection, status, temp):
                 print(status)
                 print(temp)
@@ -25,6 +32,8 @@ class load_filament_backend(threading.Thread):
         except:
             for i in range(30):
                 try:
+                    message = '{}\nFLUX{}'.format(MANTRA, i)
+                    line.send_message(_id, message)
                     print(i)
                     main.quit()
                     time.sleep(1)
