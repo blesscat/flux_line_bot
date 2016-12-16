eaab9a1f9d256a3d2cf0e28ed9ca8b118ca29803
blessma <blessma@schmidtelectronics.com>
2016-12-16 12:32:10 +0800

    test

--- app/views.py
+++ app/views.py
@@ -442,10 +442,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
+    print('event: {}'.format(dir(event)))
+    print('user id: {}'.format(dir(event.source)))
     #print('user id: {}'.format(event.source.user_id))
     #print('sender id: {}'.format(event.source.sender_id))
     #print('text: {}'.format(event.message.text))
-    print(dir(event.source))
     _id = event.source.user_id
     message = event.message.text
 
