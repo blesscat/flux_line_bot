88d251127ba0c241cb961ee3897bc6a1129da6ff
blessma <blessma@schmidtelectronics.com>
2016-12-16 11:12:50 +0800

    test

--- app/views.py
+++ app/views.py
@@ -110,7 +110,8 @@ ChannelAccessToken = os.environ.get('ChannelAccessToken')
 ChannelSecret = os.environ.get('ChannelSecret')
 
 line_bot_api = LineBotApi(ChannelAccessToken)
-handler = WebhookHandler(ChannelSecret)
+#handler = WebhookHandler(ChannelSecret)
+parser = WebhookParser(ChannelSecret)
 
 def allowed_file(filename, allowed_file):
     if allowed_file is "fc":
@@ -431,19 +432,31 @@ def callback1():
 
     # handle webhook body
     try:
-        handler.handle(body, signature)
+        events = parser.parse(body, signature)
     except InvalidSignatureError:
         abort(400)
+
+    for event in events:
+        print('event: {}'.format(event))
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def message_text(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+#@handler.add(MessageEvent, message=TextMessage)
+#def message_text(event):
+#    line_bot_api.reply_message(
+#        event.reply_token,
+#        TextSendMessage(text=event.message.text)
+#    )
 
 
 @app.route("/callback", methods=['POST'])
