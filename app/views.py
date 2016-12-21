fa75f0a037dda2220c283980df009f2036a0d009
blessma <blessma@schmidtelectronics.com>
2016-12-21 16:12:48 +0800

    test

--- app/views.py
+++ app/views.py
@@ -355,6 +355,7 @@ def index():
 def test():
     q = Queue(connection=conn)
     job = q.enqueue(count_words_at_url, 'http://heroku.com')
+    print(job.id)
     #for i in range(30):
     #    if job.status == 'finished':
     #        break
@@ -365,7 +366,7 @@ def test():
 @app.route("/test1", methods=['GET'])
 def test1():
     job = get_current_job()
-    print(dir(job))
+    print(vars(job))
     return 'ok', 200
 
 
