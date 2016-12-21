7153754111632ffeb67dde6f7619cee95866d5cb
blessma <blessma@schmidtelectronics.com>
2016-12-21 11:57:25 +0800

    test

--- app/cat.py
+++ app/cat.py
@@ -20,8 +20,8 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://172.17.0.2:6379')
 conn = redis.from_url(redis_url)
 
 FLUX_ipaddr = socket.gethostbyname(os.environ['FLUX_ipaddr'])
-while True:
-    Flux = FLUX((FLUX_ipaddr, 1901))
-    message = '{}'.format(Flux.status)
-    #line_bot_api.push_message(LINEID, TextSendMessage(text=message))
-    time.sleep(600)
+#while True:
+#    Flux = FLUX((FLUX_ipaddr, 1901))
+#    message = '{}'.format(Flux.status)
+#    #line_bot_api.push_message(LINEID, TextSendMessage(text=message))
+#    time.sleep(600)
