3ce69703dc4d3891b54043df0cd515dbbf314ed5
blessma <blessma@schmidtelectronics.com>
2016-12-21 16:59:53 +0800

    test

--- app/views.py
+++ app/views.py
@@ -367,7 +367,7 @@ def test():
 
 @app.route("/results/<job_key>", methods=['GET'])
 def get_results(job_key):
-    job = Job.fetch(job_key, cnnection=conn)
+    job = Job.fetch(job_key, connection=conn)
 
     if job.is_finished:
         return str(job.result), 200
