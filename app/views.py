02bd3e633bce4f4c1c60499c0df56e484af60b37
blessma <blessma@schmidtelectronics.com>
2016-12-22 17:44:45 +0800

    test

--- app/views.py
+++ app/views.py
@@ -14,9 +14,9 @@ from app import app
 from app.utils import assistant, LANG
 from app.exceptions import AssistReply
 from rq.job import Job
-from watchcat import conn
+#from watchcat import conn
 
-from app.utils import line_bot_api, handler#, backjob
+from app.utils import line_bot_api, handler, conn#, backjob
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
